73b7d379085dbd7b70f13df21d018df2f44623d0
AV3_08 <brandonjoash@gmail.com>
2023-11-14 08:40:01 +0000

    template

---
+++ src/client/java/me/av306/chathook/ChathookClient.java
@@ -0,0 +1,10 @@
+package me.av306.chathook;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChathookClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
